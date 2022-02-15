2277820e1f341b6a1c6d7f50a699f6abb4410ba7
SrinivasanVenkataraman <srinivasanche86@gmail.com>
2022-02-15 10:23:24 +0530

    bing

---
+++ src/test/java/bingtest/GoogeTest.java
@@ -0,0 +1,31 @@
+package bingtest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogeTest {
+
+	@Test
+	public void testGoogle() throws IOException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd=new ChromeDriver();
+		wd.get("https://www.google.com/");
+		wd.findElement(By.name("q")).sendKeys("chennai");
+		wd.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		
+		File src= ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("screenshot/google.png"));
+	}
+}
